69ac83a434ce0ff1ecc501ba57542d34acc407b6
sulavpo <sulupoudel.spp@gmail.com>
2023-08-07 08:14:33 +0545

    version 3.2

--- lib/saved_password_view/split_widget/password_list_view_widget.dart
+++ lib/saved_password_view/split_widget/password_list_view_widget.dart
@@ -1,7 +1,9 @@
+import 'package:fast_pw_manager/constants/app_icons.dart';
 import 'package:fast_pw_manager/password_gen/bloc/save_generated_data_bloc.dart';
 import 'package:fast_pw_manager/saved_password_view/split_widget/password_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PasswordListViewWidget extends StatelessWidget {
   const PasswordListViewWidget({super.key});
@@ -12,14 +14,41 @@ class PasswordListViewWidget extends StatelessWidget {
       builder: (context, state) {
         return SizedBox(
           height: 570,
-          child: ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemCount: state.savedPasswords.length,
-            itemBuilder: (context, index) {
-              final passwordData = state.savedPasswords[index];
-              return  PasswordCardWidget(passwordData: passwordData);
-            },
-          ),
+          child: state.savedPasswords.isEmpty
+              ? Center(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 50.h,
+                      ),
+                      Image.asset(
+                        AppIcons.emptyListIcon,
+                        width: 200.w,
+                        height: 200.h,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(35.sp),
+                        child: Text(
+                          'Looks like page is empty. Start adding new items',
+                          style: TextStyle(
+                            fontSize: 20.sp,
+                            color: Colors.black38,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              : ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: state.savedPasswords.length,
+                  itemBuilder: (context, index) {
+                    final passwordData = state.savedPasswords[index];
+                    return PasswordCardWidget(passwordData: passwordData);
+                  },
+                ),
         );
       },
     );
